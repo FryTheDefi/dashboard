136c441ed38994ff50e2a821833c8f82746a13f3
Viktor Valastin <viktorko99@gmail.com>
2019-08-25 09:12:57 +0200

    added something else

--- src/components/shared/ChatItem.vue
+++ src/components/shared/ChatItem.vue
@@ -7,6 +7,9 @@
   <h6 class="title is-6 message-title">{{ (user && user.name) || post.author}}</h6>
   <div>
     {{post.message}}
+    <b-button v-if="additionalData" outlined rounded @click="sendCash" type="button is-primary">
+      <strong>{{additionalData.magicWord}} {{additionalData.price}} {{ additionalData.unit }}</strong>
+    </b-button>
   </div>
   </div>
             
@@ -22,12 +25,45 @@ export default class ChatItem extends Vue {
   @Prop({ default: () => {} }) public readonly post!: any;
   @Prop({ default: () => {} }) public readonly user!: any;
 
+  // public additionalData: any = null
+  public reg = new RegExp('pay:\\d*\\D?\\d*:\\w+');
+
   private defImage: string = 'https://bulma.io/images/placeholders/128x128.png'
 
   
   public get image() : string {
     return (this.user && this.user.profileImage) || this.defImage
   }
+
+
+  public get additionalData() : any {
+    const payment = this.post.message.match(this.reg)
+    if (!payment) return null;
+    console.log(payment);
+    
+    const words = payment[0].split(':')
+    return {
+      magicWord: words[0] || 'donate',
+      price: words[1] || '0.000000000000000001',
+      unit: words[2] || 'eth'
+    }
+  }
+
+  public async sendCash() {
+    const torus = (window as any).torus;
+    await torus.ethereum.enable();
+    const to = await (window as any).torus.getPublicAddress(this.user.email)
+    
+
+
+    torus.web3.eth.sendTransaction({
+      to,
+      value: '1000000000000000'
+    }, (err) => console.log(err))
+
+
+  }
+
   
 }
 </script>
